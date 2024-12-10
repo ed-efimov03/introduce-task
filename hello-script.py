21905a03a704b5ef33916924772e00fb0c42e5c4
Ed Efimov <edefimov03@gmail.com>
2024-12-10 15:41:13 +0000

    Add python types

--- hello-script.py
+++ hello-script.py
@@ -1,1 +1,10 @@
 print("Hello lab_med_inf!")
+
+if __name__ == '__main__':  
+    print(type(3)) #<class 'int'>  
+    print(type(3.5)) #<class 'float'>  
+    print(type("Ed")) #<class 'str'>  
+    print(type(True)) #<class 'bool'>  
+    print(type([1, "Two", False])) #<class 'list'>  
+    print(type((1, False, 3.5, "Four"))) #<class 'tuple'>  
+    print(type({1: "one", 2: "two"})) #<class 'dict'>
